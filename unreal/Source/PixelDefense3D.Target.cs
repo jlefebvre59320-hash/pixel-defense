c74ba397aa5bf19eef7cc676d17dbb6b338e4a0f
jlefebvre59320-hash <jlefebvre59320@gmail.com>
2026-08-29 09:02:34 -0400

    fix: use Unreal 5.8 V7 build settings

--- unreal/Source/PixelDefense3D.Target.cs
+++ unreal/Source/PixelDefense3D.Target.cs
@@ -6,7 +6,7 @@ public class PixelDefense3DTarget : TargetRules
     public PixelDefense3DTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("PixelDefense3D");
     }
