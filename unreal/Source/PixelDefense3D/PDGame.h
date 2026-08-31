c26cba0ce415ba70fcb6b2ee5cbed28bcea8fe9d
jlefebvre59320-hash <jlefebvre59320@gmail.com>
2026-08-31 19:01:19 -0400

    Polish valley visuals from playtest feedback

--- unreal/Source/PixelDefense3D/PDGame.h
+++ unreal/Source/PixelDefense3D/PDGame.h
@@ -215,6 +215,4 @@ private:
     void DrawButton(float X,float Y,float W,float H,const FLinearColor& Color);
     void DrawLabel(const FString& Text,float X,float Y,const FLinearColor& Color,
                    float Scale=1.f);
-    UPROPERTY() UTexture2D* PanelTexture=nullptr;
-    UPROPERTY() UTexture2D* ButtonTexture=nullptr;
 };
